9d3dbf51792b296120bd196d8245dcaa0258be72
Pham Dat <trongdat1702@gmail.com>
2023-08-23 00:20:11 +0700

    fb chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,6 +37,36 @@ const App: AppType<{ session: Session | null }> = ({
                         gtag('config', 'G-VRZDT0KMJQ');`}
                     </Script>
 
+                    <div id='fb-root'></div>
+
+                    <div
+                        id='fb-customer-chat'
+                        className='fb-customerchat'
+                    ></div>
+                    <Script strategy='afterInteractive'>
+                        {`
+                            window.fbAsyncInit = function() {
+                                FB.init({
+                                xfbml            : true,
+                                version          : 'v17.0'
+                                });
+                            };
+
+                            (function(d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id)) return;
+                                js = d.createElement(s); js.id = id;
+                                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));`}
+                    </Script>
+                    <Script>
+                        {`
+                        var chatbox = document.getElementById('fb-customer-chat');
+                        chatbox.setAttribute("page_id", "113315805198292");
+                        chatbox.setAttribute("attribution", "biz_inbox");`}
+                    </Script>
+
                     <div className='flex flex-col'>
                         <HeaderComponent />
                         <div className='mt-[80px]'>
