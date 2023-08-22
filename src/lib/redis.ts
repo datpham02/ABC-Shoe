b46fb61906abe84acb567ace601f0d5c260b149b
Pham Dat <trongdat1702@gmail.com>
2023-08-22 21:22:40 +0700

    fix warning deploy

--- src/lib/redis.ts
+++
@@ -1,9 +0,0 @@
-import Redis from 'ioredis'
-
-const redis = new Redis({
-    host: process.env.NEXT_PUBLIC_REDIS_HOST,
-    port: parseInt(process.env.NEXT_PUBLIC_REDIS_PORT as string),
-    password: process.env.NEXT_PUBLIC_REDIS_PASSWORD,
-})
-
-export default redis
