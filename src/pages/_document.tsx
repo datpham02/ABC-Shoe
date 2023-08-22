ccf9c1f322023144a32bbabcd98ff72fc35a96ae
Pham Dat <trongdat1702@gmail.com>
2023-08-22 22:59:08 +0700

    fix warning

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang='en'>
-            <Head />
+            <Head>
+                <meta
+                    name='google-site-verification'
+                    content='5N-eOBBwBOKLDp78-8CI-jO1eyf4QVYVhqoL67X4YKg'
+                />
+            </Head>
 
             <body>
                 <Main />
