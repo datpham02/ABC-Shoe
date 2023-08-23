ad33893868ae843aba5216b0cb60a885a43c0579
Pham Dat <trongdat1702@gmail.com>
2023-08-24 00:21:48 +0700

    push

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    webpack(config, { isServer }) {
-        if (isServer) {
-            generateRobotsTxt()
-        }
-        return config
-    },
 }
 
 module.exports = nextConfig
